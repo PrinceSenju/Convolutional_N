a488ec9f27ca07a163be0c0305fb0c2725737b34
Volodymyr Kindratenko <kindr@iridium.ncsa.illinois.edu>
2019-06-04 14:58:01 -0500

    initial files

---
+++ libcorr.c
@@ -0,0 +1,17 @@
+#include "libcorr.h"
+
+void corr2d(int *X, int Wx, int Hx, int *K, int w, int h, int *Y)
+{
+
+/* implement me */
+
+}
+
+
+void print2d(int *X, int Wx, int Hx)
+{
+
+/* implement me */
+
+}
+
---
+++ libcorr.h
@@ -0,0 +1,9 @@
+#ifndef _LIB_CORR_H_
+#define _LIB_CORR_H_
+
+void corr2d(int *X, int Wx, int Hx, int *K, int w, int h, int *Y);
+
+void print2d(int *X, int Wx, int Hx);
+
+
+#endif
