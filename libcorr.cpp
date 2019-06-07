a9edd5d86e612d19b9594255b81b1553ebeeaa01
Volodymyr Kindratenko <kindr@iridium.ncsa.illinois.edu>
2019-06-07 10:59:46 -0500

    All on C++ now

---
+++ libcorr.cpp
@@ -0,0 +1,53 @@
+
+/*
+ * input: X - image, Hx, Wx - image size
+          K - kernel, h, w - kernel size
+ * output: Y - image of size (Wx - w + 1) by (Hx - h + 1)
+ why should  we use flop
+	figure out what operations can be floating operationsx
+ */
+
+template <class T, class U, class V>
+void  corr2d(T *X, unsigned Wx, unsigned Hx, T *K, unsigned w, unsigned h, T *Y, unsigned *flop) 
+{
+    unsigned row, col;
+    unsigned i, j;
+
+    unsigned Wy = Wx - w + 1;
+    unsigned Hy = Hx - h + 1;
+
+    *flop = Wy * Hy * w * h;
+
+    for (row = 0; row < Hy; row++) {
+        for (col = 0;  col < Wy; col++) {
+            *(Y + row * Wy + col) = 0;
+            for (i = 0; i < h; i++) {
+                for (j = 0; j < w; j++) {  
+                 	//flops+=2;
+			        *(Y + row *Wy + col) += *(X + (row + i) * Wx + (col + j)) * (*(K + i * w + j));
+                }
+            }
+         }
+     }
+}
+
+template <class T>
+void print2d(T *X, unsigned Wx, unsigned Hx)
+{
+   unsigned x, y;
+
+   for (y = 0; y < Hx; y++) {
+      for (x = 0; x < Wx; x++) {
+            std::cout << *(X+y*Wx+x) << " ";
+      }
+      std::cout << std::endl;
+   }
+}
+
+template <class T>
+void random2d(T *X, unsigned Wx, unsigned Hx)
+{
+// @implement me: fill in array with random numbers
+
+}
+
---
+++ test.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h> 
+#include <sys/time.h>
+#include <unistd.h>
+#include "libcorr.h"
+
+void simple_test() 
+{
+    struct timeval start_clk, end_clk;
+    double elapsed;
+
+    const unsigned Wx = 3;
+    const unsigned Hx = 3;
+    float X[3*3] = { 0, 1, 2,   3, 4, 5,   6, 7, 8 };
+
+    const unsigned w = 2;
+    const unsigned h = 2;
+    float K[2*2] = { 0.0, 1.0,  2.0, 3.0 };
+
+    const unsigned Wy = Wx - w + 1;
+    const unsigned Hy = Hx - h + 1;
+    float Y[2*2];
+
+    unsigned flop = 0;
+
+    std::cout << "Input matrix: " << std::endl;
+    print2d<float>(X, Wx, Hx);
+    std::cout << std::endl;
+
+    std::cout << "Kernel: " << std::endl;
+    print2d<float>(K, w, h);
+    std::cout << std::endl;
+
+    gettimeofday(&start_clk, NULL); /// get the start time
+
+    corr2d<float, float, float>(X, Wx, Hx, K, w, h, Y, &flop);
+
+    gettimeofday(&end_clk, NULL); /// get the end time
+
+    std::cout << "Output matrix: " << std::endl;
+    print2d<float>(Y, Wy, Hy);
+    std::cout << std::endl;
+
+    //std::cout << "Start time: " << start_clk << std::endl;
+    //std::cout << "End time: " << end_clk << std::endl;
+    std::cout << "Number of floating-point operations: " << flop << std::endl;    
+    elapsed = (end_clk.tv_sec - start_clk.tv_sec) + (end_clk.tv_usec - start_clk.tv_usec) / 1000000.0;
+    std::cout << "Elapsed time: " << elapsed << std::endl;    
+    std::cout << "FLOPS: " << flop / elapsed << std::endl;
+}
+
+void more_complex_tes()
+{
+
+    const unsigned Wx = 120;
+    const unsigned Hx = 120;
+    float *X = new float[Wx * Hx];
+
+    random2d<float>(X, Wx, Hx); 
+
+    // @implement me
+
+}
+
+
+// include header function time and input
+int main() {
+
+    simple_tes();
+
+    more_complex_tes();
+
+    return 0;
+}
