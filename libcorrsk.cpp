e13da47b00058d8d7b8be46b53da2ddbac2661ef
PrinceSenju <prince.ghartey@gmail.com>
2019-06-19 11:12:51 -0500

    help

--- libcorrsk.cpp
+++ libcorrsk.cpp
@@ -10,6 +10,36 @@ template <class T, class U, class V>
 void corrSK(T *X, unsigned Wx, unsigned Hx, U *Krow, U *Kcol, unsigned w, unsigned h, V *Y, unsigned *flop)
 {
 // implement me
+// find center position of kernel (half of kernel size)
+kCenterX = kCols / 2;
+kCenterY = kRows / 2;
+
+for(i=0; i < rows; ++i)              // rows
+{
+    for(j=0; j < cols; ++j)          // columns
+    {
+        sum = 0;                     // init to 0 before sum
+
+        for(m=0; m < kRows; ++m)     // kernel rows
+        {
+          horizontal  = kRows - 1 - m;      // row index of flipped kernel
+
+            for(n=0; n < kCols; ++n) // kernel columns
+            {
+                vertical = kCols - 1 - n;  // column index of flipped kernel
+
+                // index of input signal, used for checking boundary
+                ii = i + (m - kCenterY);
+                jj = j + (n - kCenterX);
+
+                // ignore input samples which are out of bound
+                if( ii >= 0 && ii < rows && jj >= 0 && jj < cols )
+                out[i][j] += in[ii][jj] * kernel[horizontal][ vertical];
+            }
+        }
+    }
+}
+
 }
 
 
